d84ff03e7a52d29c6a05d6698e19d4b232e97c65
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-10-24 17:39:52 +0200

    Issue #1

--- shoppingcart.php
+++ shoppingcart.php
@@ -14,21 +14,25 @@ function showShoppingcartContent($data){
         foreach($data['shoppingcartproducts'] as $product) {  // echo '<p> ID:' . $productId . '</p>';
             echo '<div class="product"><a href="index.php?page=detail&id=' . $product['productId'] . '">';
             echo '<h2>' . $product['name'] . '</h2>';
-            echo '<img src="Images/' . $product['filename_img'] . '" alt="' . $product['name'] . '" width="60" height="80"><br></a>' . PHP_EOL;
+            echo '<img src="Images/' . $product['filename_img'] . '" alt="' . $product['name'] . '" width="60" height="80"></a>' . PHP_EOL;
+            echo '<div class="text">';
             echo '<div class="amount">';
-            echo '<p4> Hoeveelheid: ';
+            echo '<p> Hoeveelheid:&nbsp;</p>';
             addAction('shoppingcart', 'addToShoppingcart', "+", $product['productId'], $product['name'], 1);
             echo $product['quantity'];
             addAction('shoppingcart', 'addToShoppingcart', "-", $product['productId'], $product['name'], -1);
-            echo '</p4></div><br>' . PHP_EOL;
-            echo '<p4> Subtotaal: &euro;' . $product['subtotal'] . ' </p4>';
+            echo '</div>'. PHP_EOL;
+            echo '<div class="subtotal"><p> Subtotaal: &euro;' . $product['subtotal'] . '</p></div><br>';
+            echo '<div class="removebutton">';
             addAction('shoppingcart', 'removeFromShoppingcart', "Remove from shoppingcart", $product['productId'], $product['name'], 0);
-            echo '</div>';
+            echo '</div></div></div>';
         }
-        echo '<p4>Totaal: &euro;' . $data['total'] .  '</p4>';
+        echo '<div class="total">';
+        echo '<p>Totaal: &euro;' . $data['total'] .  '</p>';
         addAction('home','order', "Order");
+        echo '</div>';
     } else {
-        echo '<p4>Je shoppingcart is nog leeg.</p4>';
+        echo '<p>Je shoppingcart is nog leeg.</p>';
     }
     echo '</div>'; 
 }
--- webshop.php
+++ webshop.php
@@ -13,11 +13,13 @@ function showWebshopContent($data) {
     foreach($data['products'] as $product) {
     echo '<div class="product"><a href="index.php?page=detail&id=' . $product['id'] . '">';
     echo '<h2>' . $product['name'] . '</h2>';
-    echo '<img src="Images/' . $product['filename_img'] . '" alt="' . $product['name'] . '" width="60" height="80"><br>';
-    echo '<p4>Id: ' . $product['id'] . '</p4><br>';
-    echo '<p5>Prijs: &euro;' . $product['price'] . '</p5></a>';
+    echo '<img src="Images/' . $product['filename_img'] . '" alt="' . $product['name'] . '" width="60" height="80"></a>'. PHP_EOL;
+    echo '<div class="text">';
+    echo '<div class="id"><p>Id: ' . $product['id'] . '</p></div>'. PHP_EOL;
+    echo '<div class="price"><p>Prijs: &euro;' . $product['price'] . '</p></div><br>';
+    echo '<div class="addbutton">';
     addAction('webshop', 'addToShoppingcart', "Add to shoppingcart", $product['id'], $product['name'], 1);
-    echo '</div>'. PHP_EOL;
+    echo '</div></div></div>'. PHP_EOL;
     }
     echo '</div>';  
 }
